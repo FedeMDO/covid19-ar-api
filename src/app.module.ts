4e823dc169db9ddb2de19fa14d7da84b90e73140
FedeMDO <federicomdo97@gmail.com>
2020-07-09 01:19:02 +0200

    Feat: support bd connection in prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     }),
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
+        uri:
+          process.env.MONGODB_URI || configService.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
     }),

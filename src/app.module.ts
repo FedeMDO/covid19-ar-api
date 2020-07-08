5559ae5c0aa190b99b3b198e0756580fffe5c27f
FedeMDO <federicomdo97@gmail.com>
2020-07-09 01:39:13 +0200

    heroku deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         uri:
-          configService.get<string>('MONGODB_URI') || process.env.MONGODB_URI,
+          process.env.MONGODB_URI || configService.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
     }),

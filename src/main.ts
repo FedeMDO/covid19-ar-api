e5136398a87b6bc9049832a67402d1001a085d59
FedeMDO <federicomdo97@gmail.com>
2020-07-09 02:40:10 +0200

    Documentation: openapi swagger docs at /api endp.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+  // app.setGlobalPrefix('v1');
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('COVID19 Status Argentina')
+    .setDescription(
+      'API de casos de COVID19 en Argentina a nivel nacional, provincial y departamental. Las fechas deben pasarse en formato Date ISO 8601 (yyyy-mm-dd)',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 80);
 }
 bootstrap();
